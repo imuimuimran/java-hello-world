3628825f595414227bdd25b0d1e2c5986455405e
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-06 17:07:10 +0600

    add string input

---
+++ java_input/UserNameInput.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+class UserNameInput  {  
+    public static void main(String[] args)  {  
+        Scanner sc= new Scanner(System.in);  
+        System.out.print("Enter Your Name: ");  
+        String str= sc.nextLine();       
+        System.out.print("Your Name is: "+str);             
+    }  
+}  
