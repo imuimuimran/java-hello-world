a1f751dae428d683b98c97fb02c2899851cffccf
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-07 10:40:06 +0600

    add basic conditional statement

---
+++ conditional statements/ConditionalifElse.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+class ConditionalifElse  {  
+    public static void main(String[] args)  {  
+        Scanner sc= new Scanner(System.in);  
+        System.out.print("Enter Your age: ");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You are adult.");
+        } else {
+            System.out.println("You are not adult.");
+        }          
+    }  
+}  
