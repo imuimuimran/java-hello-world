29ca7f6a014fe83a8c73fa200ed4e09673f9d79f
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-13 18:11:58 +0600

    add a function directly where two number can multiply.

--- functions_in_java/multiplyTwoNumber.java
+++ functions_in_java/multiplyTwoNumber.java
@@ -1,8 +1,7 @@
 import java.util.*;  
-public class sumofTwoNumbers {  
-    public static int calculetSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+public class multiplyTwoNumber {  
+    public static int productCalculet(int a, int b) {
+        return a * b;
     }
     public static void main(String[] args)  {  
         Scanner scanner = new Scanner(System.in);  
@@ -11,8 +10,6 @@ public class sumofTwoNumbers {
         System.out.print("Enter Second Number: ");
         int b = scanner.nextInt(); 
 
-        int sum = calculetSum(a, b);
-
-        System.out.println("The sum of two number is: " + sum);
+        System.out.println("The product of two number is: " + productCalculet(a, b));
     }  
 }  
