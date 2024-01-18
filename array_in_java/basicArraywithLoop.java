bfb545b272113197d82a24ba47cdcc8383dcd87a
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-18 14:56:11 +0600

    This is a template of Basic array with loop.

---
+++ array_in_java/basicArraywithLoop.java
@@ -0,0 +1,13 @@
+public class basicArraywithLoop {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+
+        marks[0] = 97;
+        marks[1] = 96;
+        marks[2] = 98;
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
