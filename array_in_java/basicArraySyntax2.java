5b6c59c0f21d0884a5a6bf012e8fce0662becca1
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-19 13:39:09 +0600

    add a file which is about basic syntax of array.

---
+++ array_in_java/basicArraySyntax2.java
@@ -0,0 +1,12 @@
+// import java.util.*;
+
+public class basicArraySyntax2 {
+
+    public static void main(String[] args) {
+        int[] marks = {97, 96, 98};
+
+        System.out.println("Math: " + marks[0]);
+        System.out.println("Java: " + marks[1]);
+        System.out.println("Python: " + marks[2]);
+    }
+}
