51ffedaab4a8c9c75527bd645d9353e86ee37dbe
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-22 13:37:58 +0600

    add string concatenation in Java.

---
+++ string_in_java/stringConcatenation.java
@@ -0,0 +1,9 @@
+class stringConcatenation  {  
+    // Concatenation
+    public static void main(String[] args)  {      
+        String firstName = "Mohd. Imran";
+        String lastName = "Hossain";
+        String fullName= firstName + " " + lastName;       
+        System.out.print(fullName);     
+    }  
+}  
