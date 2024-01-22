7ff1edbfe9404d7d34780d824f7a4cb426180391
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-22 15:45:39 +0600

    add length method in string.

---
+++ string_in_java/LengthinString.java
@@ -0,0 +1,9 @@
+class LengthinString  {  
+    // Concatenation
+    public static void main(String[] args)  {      
+        String firstName = "Mohd. Imran";
+        String lastName = "Hossain";
+        String fullName= firstName + " " + lastName;       
+        System.out.print(fullName.length());     
+    }  
+}  
