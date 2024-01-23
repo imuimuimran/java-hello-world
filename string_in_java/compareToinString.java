a24e448eb1e37e70282c2c94f1b0e34b4d7b7a01
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-23 13:15:17 +0600

    add how to compare strings.

---
+++ string_in_java/compareToinString.java
@@ -0,0 +1,13 @@
+class compareToinString  {  
+    // Compare
+    public static void main(String[] args)  {      
+        String name1 = "Imran";
+        String name2 = "Imran";
+        
+        if(name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal.");
+        }else {
+            System.out.println("Strings are not equal.");
+        }
+    }  
+}  
