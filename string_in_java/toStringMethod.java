94dd5e8d8601b92cabd28cc06e955bb20639378b
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-23 14:14:40 +0600

    add a program where number changes into string by toString method.

---
+++ string_in_java/toStringMethod.java
@@ -0,0 +1,9 @@
+public class toStringMethod {
+    public static void main(String args[]) {
+        int number = 123;
+        String str = Integer.toString(number);
+
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
