19bfbbca5d91fbc880c670d2fa208f0e838a728e
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-23 13:36:34 +0600

    add basic parseInt method where string will change into number.

---
+++ string_in_java/parseIntMethod.java
@@ -0,0 +1,11 @@
+public class parseIntMethod {
+    
+    // String to Intiger
+    public static void main(String args[]) {
+        String str = "123456";
+        int number = Integer.parseInt(str);
+
+        System.out.println(number);
+
+    }
+}
