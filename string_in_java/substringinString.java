2eaafb7bc80acf8bc90cb6929eb54bd7980bd741
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-23 13:17:10 +0600

    add a file in java where substring of a string can display.

---
+++ string_in_java/substringinString.java
@@ -0,0 +1,8 @@
+class substringinString  {  
+    public static void main(String[] args)  {      
+        String sentence = "Mohd. Imran Hossain";
+        // String name= sentence.substring(12, sentence.length()); // Till Last of the Index.
+        String name= sentence.substring(6, 11);       
+        System.out.print(name);     
+    }  
+}  
