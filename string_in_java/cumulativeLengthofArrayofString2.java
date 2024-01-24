21397da000a35cfb99a338d25550b121c97ee8b5
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-24 14:10:22 +0600

    This program can count the number of total inputed string.

---
+++ string_in_java/cumulativeLengthofArrayofString2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class cumulativeLengthofArrayofString2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Enter the size of array of string: ");
+        int size = sc.nextInt();
+        
+        String array[] = new String[size];
+        int totalLength = 0;
+
+        // Input
+        System.out.print("Input the arrays of string: ");
+        for(int i=0; i<size; i++) {
+            array[i] = sc.nextLine();
+            totalLength += array[i].length();
+        }
+
+        System.out.println("The length of cumulative string is: " + totalLength);
+
+        sc.close();
+    }
+}
