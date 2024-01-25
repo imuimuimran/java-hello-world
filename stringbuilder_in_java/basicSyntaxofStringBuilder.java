6aaf682fc90531c88b4e7ac4cb0887294c25ddb5
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-25 14:53:54 +0600

    add the basic syntax of string builder.

---
+++ stringbuilder_in_java/basicSyntaxofStringBuilder.java
@@ -0,0 +1,7 @@
+public class basicSyntaxofStringBuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Imran Hossain");
+
+        System.out.println(sb); 
+    }
+}
