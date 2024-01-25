9f2044c1486ea97d093f022a4ec8e510cc91a30e
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-25 15:00:22 +0600

    this program can print the character of any index position of string builder.

---
+++ stringbuilder_in_java/charAtinStringBuilder.java
@@ -0,0 +1,10 @@
+public class charAtinStringBuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Imran Hossain");
+
+        System.out.println(sb); 
+
+        // Print charAt index 2 position
+        System.out.println(sb.charAt(2));
+    }
+}
