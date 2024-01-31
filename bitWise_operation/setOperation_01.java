5baf53ad3186700c16eeeef223754c5e9c9af7d3
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-31 20:57:29 +0600

    This program can diplay the output of bit set operation.

---
+++ bitWise_operation/setOperation_01.java
@@ -0,0 +1,16 @@
+public class setOperation_01{
+    public static void main(String[] args) {
+        int num = 5; // the binary of decimal 5 is 0101
+        int position = 2; // so the 2nd (0101) or left side position 2 is 1
+
+        int bitMast = 1 << position;
+
+        if((bitMast & num) == 0) {
+            System.out.println("number is zero");
+        }else {
+            System.out.println("number is one.");
+        }
+    
+    }
+
+}
